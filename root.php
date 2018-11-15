271f349e385c90422c33b505ba09f3f5da1539ce
Ricardo Cantó <rifcaab@gmail.com>
2018-11-15 23:23:10 +0100

    corregint root_template faltava coma

--- root.php
+++ root.php
@@ -3,7 +3,7 @@
 // Esta función escuchará en la ruta principal de la API, '/'
 $app->get('/', function ($req, $res, $args) {
 
-    // Creamos un objeto collection + json con cuatro enlaces a las cuatro categorías
+    // Creamos un objeto collection + json con cinco enlaces a las cinco categorías
 
     // Creamos los datos que vamos a pasar a la vista
     return $this->view->render($res, 'root_template.php', [
