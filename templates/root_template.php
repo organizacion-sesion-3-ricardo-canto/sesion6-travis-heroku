7e68a2d8a007e2ae55c8f490a4ced3a1cc18815d
Ricardo Cantó Abad <rcanto@users.noreply.github.com>
2018-11-15 09:31:01 +0100

    Update root_template.php
    Afegint tvseries al collection

--- templates/root_template.php
+++ templates/root_template.php
@@ -10,6 +10,7 @@
                 {"rel" : "collection", "href" : "{{ path_for('books') }}","prompt":"Books"},
                 {"rel" : "collection", "href" : "{{ path_for('musicalbums') }}","prompt":"Music Albums"},
                 {"rel" : "collection", "href" : "{{ path_for('games') }}","prompt":"Videogames"}
+                {"rel" : "collection", "href" : "{{ path_for('tvseries') }}","prompt":"TVseries"}
             ]
             }
 }	
